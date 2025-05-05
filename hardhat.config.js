2dad825d183b9b6d2de10891d95b336681454d9a
Lamont <missionpossible23@protonmail.com>
2025-05-05 09:58:42 -0400

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,10 @@
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: '0.8.28',
+  networks: {
+    bsc: {
+      url: 'INSERT_RPC_API_ENDPOINT', // Insert your RPC URL here
+      chainId: 56,
+      accounts: ['INSERT_PRIVATE_KEY'],
+    },
+  },
 };
